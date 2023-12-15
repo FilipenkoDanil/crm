b1ce0bddc42f2bd1434a914fee10b8e0a9c9be30
FilipenkoDanil <dan.filipenko@yandex.ru>
2023-12-15 12:07:30 +0200

    Vue configuration

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<template>
+    <v-app>
+
+    </v-app>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/vuetify.js
@@ -0,0 +1,12 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives
+})
+
+export default vuetify
